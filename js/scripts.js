cf1e449c29dba2c1279bc39256eb580868b6eb08
samstoia <samstoia@gmail.com>
2019-01-18 14:42:33 -0800

    It will add jQuery operating to scroll to bottom of webpage after user clicks submit button

--- js/scripts.js
+++ js/scripts.js
@@ -27,21 +27,25 @@ function match(total) {
   $("#cssShow").show();
   $("#javaShow").hide()
   $("#csharpShow").hide()
+  $('html, body').animate({scrollTop:$(document).height()}, 'slow');
   }
 
   else if (total > 7 && total < 11) {
   $("#javaShow").show();
   $("#cssShow").hide();
   $("#csharpShow").hide()
+  $('html, body').animate({scrollTop:$(document).height()}, 'slow');
   }
 
   else if (total > 10) {
-  $("#csharpShow").show();
+  $("#csharpShow").slideDown();
   $("#javaShow").hide();
   $("#cssShow").hide();
+  $('html, body').animate({scrollTop:$(document).height()}, 'slow');
   }
 
   else if (total != 6, 7, 8, 9, 10, 11, 12) [
     alert("Please Make Sure All Questions are Answered!")
   ]
+
 };
